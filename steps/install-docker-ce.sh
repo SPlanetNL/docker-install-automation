3e19dfd22dc4011932b4420a1a947c71c3b4bb38
Colin Gelling <colingelling96@gmail.com>
2021-12-07 20:10:16 +0100

    Corrections for completing installation

--- steps/install-docker-ce.sh
+++ steps/install-docker-ce.sh
@@ -4,10 +4,10 @@
 # this installation must be ran with root permissions
 
 # prepare, making sure that the OS is up-to-date
-apt-get update && apt-get upgrade -qy
+sudo apt-get update && apt-get upgrade -qy
 
 # setup the repository
-apt-get install -qy \
+sudo apt-get install -qy \
     ca-certificates \
     curl \
     gnupg \
@@ -19,15 +19,15 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 # get the repository
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # install Docker CE
-apt-get update && apt-get install -qy docker-ce docker-ce-cli containerd.io
+sudo apt-get update && apt-get install -qy docker-ce docker-ce-cli containerd.io
 
 # verify installation by running one container
-docker run --name="hello-world" -d hello-world
+sudo docker run --name="hello-world" -d hello-world
 
-if grep -q "$(docker ps -a | grep hello-world)"; then
+if [[ "$(docker ps -a | grep "hello-world")" ]]; then
 
   echo && echo "Docker has been installed successfully!"
 
