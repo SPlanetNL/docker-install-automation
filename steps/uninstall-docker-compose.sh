e991f368ba4c96c3aa525b67fb1a9ad93e2e6375
Colin Gelling <22787309+colingelling@users.noreply.github.com>
2024-10-27 13:18:35 +0100

    Improved uninstallation of docker-compose execution commands

--- steps/uninstall-docker-compose.sh
+++ steps/uninstall-docker-compose.sh
@@ -4,3 +4,9 @@
 
 apt-get purge docker-compose-plugin -y \
   && apt-get autoremove docker-compose-plugin -y
+
+rm -f "$(which docker-compose)"
+
+if [[ -z "$(command -v docker-compose --version &> /dev/null)" ]]; then
+  echo && echo "Successfully uninstalled docker-compose"
+fi
