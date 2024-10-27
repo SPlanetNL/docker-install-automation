deb80e55788cdbe9735dc0829b64af7f46a8693b
Colin Gelling <22787309+colingelling@users.noreply.github.com>
2024-10-27 10:55:15 +0100

    Minor corrections, slightly altered project structure

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 main() {
 
-  source "steps/install/docker-ce.sh"
-  source "steps/install/docker-compose.sh"
-  source "steps/configure/post-installation.sh"
+  source "steps/installation-scripts/docker-ce.sh"
+  source "steps/installation-scripts/docker-compose.sh"
+  source "steps/installation-scripts/post-installation.sh"
 
 }
 
